d61f22963ec038ceaaefa797ae56340ff193319f
Garrett Amini <kadjar@gmail.com>
2013-03-15 15:11:49 -0700

    fixed reference bug

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -6,6 +6,23 @@ function AppCtrl($scope, toto) {
 }
 
 function MyCtrl1($scope, toto) {
+  function updateCount() {
+    toto.request("count", {}).then(function(response){
+      $('#output').append($('<p>').text(response.count));
+    });
+  }
+
+  function updateUI() {
+    if (toto.sessionID()) {
+      $('#unauth').addClass('hidden');
+      $('#logged-in').removeClass('hidden');
+      updateCount();
+    } else {
+      $('#unauth').removeClass('hidden');
+      $('#logged-in').addClass('hidden');
+    }
+  }
+  
   $('#login').on('click', function () { 
     toto.request('account.login', {'user_id': $('#email').val(), 'password': $('#pass').val()}).then(function(){
       updateUI();
@@ -37,19 +54,3 @@ function MyCtrl2() {
 MyCtrl2.$inject = [];
 
   
-function updateCount() {
-  toto.request("count", {}).then(function(response){
-    $('#output').append($('<p>').text(response.count));
-  });
-}
-
-function updateUI() {
-  if (toto.sessionID()) {
-    $('#unauth').addClass('hidden');
-    $('#logged-in').removeClass('hidden');
-    updateCount();
-  } else {
-    $('#unauth').removeClass('hidden');
-    $('#logged-in').addClass('hidden');
-  }
-}
